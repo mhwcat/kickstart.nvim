1e6da50b2acf1252cb125624c6d7c16a285cb6b1
mhwcat <mhwcat@gmail.com>
2024-06-29 15:08:26 +0200

    Add customization for terminal

--- init.lua
+++ init.lua
@@ -206,6 +206,9 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+vim.api.nvim_create_autocmd({ 'TermOpen', 'WinEnter' }, { pattern = 'term://*', command = 'startinsert' })
+vim.api.nvim_create_autocmd('VimEnter', { pattern = '*', command = 'split | resize 10 | terminal' })
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
